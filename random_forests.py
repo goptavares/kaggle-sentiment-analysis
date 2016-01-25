3c38e43dcee120c7de41de80442a78ea5cc9f998
Juri Minxha <jminxha@gmail.com>
2016-01-25 12:08:21 -0800

    Tried Random Forests

---
+++ random_forests.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+random_forests.py
+Authors: Gabriela Tavares,      gtavares@caltech.edu
+         Juri Minxha,           jminxha@caltech.edu
+"""
+
+from sklearn.cross_validation import cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+import util
+
+# Load the training data
+X_train, Y_train, X_test = util.loadData()
+clf    = RandomForestClassifier(n_estimators=200, max_depth=None, min_samples_split=1,random_state=0)
+scores = cross_val_score(clf,X_train,Y_train) 
+print(scores.mean())
+
+
+"""
+It seems that the cross validated score reaches a maximum score of ~0.7
+"""
